7b0846533b5ae43ecca12dd1a12cc5d576548068
Meacha1 <meachattd@gmail.com>
2023-11-04 17:05:44 +0300

    url

--- cad_market/cad_market/settings.py
+++ cad_market/cad_market/settings.py
@@ -211,11 +211,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -227,10 +222,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 # Make them publicly accessible at this URL endpoint  
 MEDIA_URL = '/files/'
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-#STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 REST_FRAMEWORK = {
