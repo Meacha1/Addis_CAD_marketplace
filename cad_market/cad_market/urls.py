c5f16d29dcec28eaa6e9a10fe9c1a6f8566f573f
Meacha1 <meachattd@gmail.com>
2023-11-04 16:50:48 +0300

    url_old

--- cad_market/cad_market/urls.py
+++ cad_market/cad_market/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('auth/', include('djoser.social.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
