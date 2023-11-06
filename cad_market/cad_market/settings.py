99924b558f2bb72f4a40aec3210a6eb1f95169e4
Meacha1 <meachattd@gmail.com>
2023-11-06 20:15:56 +0300

    test15

--- cad_market/cad_market/settings.py
+++ cad_market/cad_market/settings.py
@@ -233,7 +233,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
