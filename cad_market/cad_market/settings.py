d440a0a2f5373f6fce11f7f1c7a6c33341c238fb
Meacha1 <meachattd@gmail.com>
2023-11-07 13:43:39 +0300

    test7

--- cad_market/cad_market/settings.py
+++ cad_market/cad_market/settings.py
@@ -223,7 +223,7 @@ MEDIA_URL = '/files/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
